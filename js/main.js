515903dca0065a559fea18714771ea89cee505cf
TheInvoker <ryans.dsouza@mail.utoronto.ca>
2016-07-23 01:55:37 -0400

    resume

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
 	});
 	$(".menu .menu-header").on('click', '.menu-item', function() {
 		$(".menu").removeClass("open");
+		$(".menu-item.selected").removeClass("selected");
+		$(this).addClass("selected");
 		return false;
 	});
 });
