2e93954c5bb9567285a3ae3e5aa886bd8ac292f1
TheInvoker <rydsouza82@gmail.com>
2019-07-03 11:42:09 -0400

    update

--- pong.js
+++ pong.js
@@ -20,6 +20,7 @@ function gLoop() {
             buffer_xs.push([item[0]/game_md.width, item[1]/game_md.height, normalizeDirection(item[2])/Math.PI]);
             buffer_ys.push(ball.y/game_md.height);
         });
+        buffer = [];
         if (buffer_xs.length > 100) {
             postMessage([buffer_xs, buffer_ys]);
             buffer_xs = [];
